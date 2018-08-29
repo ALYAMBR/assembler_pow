31108314f251766ca002ef1c6616d1c8a17bcd9e
ALYAMBR <31818444+ALYAMBR@users.noreply.github.com>
2018-08-29 12:33:12 +0700

    Final.

--- pow.asm
+++ pow.asm
@@ -3,14 +3,20 @@
 main:
     movq %rsp, %rbp #for correct debugging
 pow:
-    movq $0, %rsi #p - степень, тестовый ввод
-    movq $0, %rbx
-    movq $2, %rdi #x - число, тестовый ввод
     cmp $0, %rsi
     je p_zero
     cmp $0, %rdi
     je x_zero
-    
+    movq %rdi, %rax
+    jmp pow_cycle
+finish:
+    retq
+pow_cycle:
+    sub $1, %rsi
+    cmp $0, %rsi
+    je finish
+    imul %rdi, %rax
+    jmp pow_cycle
 p_zero:
     movq $1, %rax
     retq
